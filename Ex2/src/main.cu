73e8b6c0c00610ef25000c6b8664f638120cdf3c
ChristianGollmann7 <christian.gollmann7@gmail.com>
2022-05-09 08:43:13 +0000

    copied adams stuff into my branch (graph generator script and graph class)

--- Ex2/src/main.cu
+++ Ex2/src/main.cu
@@ -1,8 +1,13 @@
 #include <iostream>
 
+#include "graph.hpp"
+
 int main()
 {
     std::cout << "Hello, World" << std::endl;
-    
+    Graph input("10_0.2_1_100.csv");
+    input.getCOOReepresentation();
+    input.getCSRRepresentation();
+    input.getELLRepresentation();
     return 0;
 }
