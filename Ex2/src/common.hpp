0bab401f5d3faded4b740b30441217925aa5ae62
ChristianGollmann7 <christian.gollmann7@gmail.com>
2022-05-20 16:35:01 +0000

    added file option

--- Ex2/src/common.hpp
+++ Ex2/src/common.hpp
@@ -1,1 +1,5 @@
 #define EXIT_SUCCESS 0
+
+
+// Options
+#define DEFAULT_INPUT_FILE "10_0.2_1_100.csv"
--- Ex2/src/main.cu
+++ Ex2/src/main.cu
@@ -1,4 +1,7 @@
+#include "Graph.hpp"
+#include "common.hpp"
 #include <iostream>
+#include <string>
 
 #include <boost/program_options.hpp>
 
@@ -11,19 +14,26 @@ OptionsT parse_options(int ac, char **av)
 
     // clang-format off
     desc.add_options()
-        ("help,h", "produce help message");
+        ("help,h", 
+        "produce help message")
 
+        ("inputfile,i", 
+        po::value<std::vector<std::string>>(), 
+        "input file containing graph data");
     // clang-format on
 
+    // Bioler-plate Boost options stuff
     po::variables_map vm;
     po::store(po::parse_command_line(ac, av, desc), vm);
     po::notify(vm);
 
+    // Produce help message
     if (vm.count("help"))
     {
         std::cout << desc << "\n";
         exit(EXIT_SUCCESS);
     }
+
 
     return vm;
 }
@@ -31,6 +41,13 @@ OptionsT parse_options(int ac, char **av)
 int main(int ac, char **av)
 {
     OptionsT options = parse_options(ac, av);
-    
+
+    // I havent found a good way for adding defaults tring to boost::options so im doing it by hand
+    std::string input_file = options.count("inputfile")
+                                  ? options["inputfile"].as<std::vector<std::string>>()[0]
+                                  : DEFAULT_INPUT_FILE;
+
+    std::cout << "Loading " << input_file << std::endl;
+
     return 0;
 }
