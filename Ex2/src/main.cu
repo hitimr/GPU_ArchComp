1c4f9d44e6844ef52ef4ca7ecb1ada61f9634e41
hitimr <hitimr@gmail.com>
2022-05-20 21:06:16 +0200

    input file

--- Ex2/src/main.cu
+++ Ex2/src/main.cu
@@ -10,9 +10,9 @@ using OptionsT = boost::program_options::variables_map;
 
 OptionsT parse_options(int ac, char **av)
 {
-    po::options_description desc("Allowed options");
+  po::options_description desc("Allowed options");
 
-    // clang-format off
+  // clang-format off
     desc.add_options()
         ("help,h", 
         "produce help message")
@@ -20,34 +20,34 @@ OptionsT parse_options(int ac, char **av)
         ("inputfile,i", 
         po::value<std::vector<std::string>>(), 
         "input file containing graph data");
-    // clang-format on
+  // clang-format on
 
-    // Bioler-plate Boost options stuff
-    po::variables_map vm;
-    po::store(po::parse_command_line(ac, av, desc), vm);
-    po::notify(vm);
+  // Bioler-plate Boost options stuff
+  po::variables_map vm;
+  po::store(po::parse_command_line(ac, av, desc), vm);
+  po::notify(vm);
 
-    // Produce help message
-    if (vm.count("help"))
-    {
-        std::cout << desc << "\n";
-        exit(EXIT_SUCCESS);
-    }
+  // Produce help message
+  if (vm.count("help"))
+  {
+    std::cout << desc << "\n";
+    exit(EXIT_SUCCESS);
+  }
 
-
-    return vm;
+  return vm;
 }
 
 int main(int ac, char **av)
 {
-    OptionsT options = parse_options(ac, av);
+  OptionsT options = parse_options(ac, av);
 
-    // I havent found a good way for adding defaults tring to boost::options so im doing it by hand
-    std::string input_file = options.count("inputfile")
-                                  ? options["inputfile"].as<std::vector<std::string>>()[0]
-                                  : DEFAULT_INPUT_FILE;
+  // I havent found a good way for adding defaults tring to boost::options so im doing it by hand
+  std::string input_file = options.count("inputfile")
+                               ? options["inputfile"].as<std::vector<std::string>>()[0]
+                               : DEFAULT_INPUT_FILE;
 
-    std::cout << "Loading " << input_file << std::endl;
+  std::cout << "Loading " << input_file << std::endl;
+  
 
-    return 0;
+  return 0;
 }
