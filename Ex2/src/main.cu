4a45c2af43b03a4c3bf66c54247e0d98bcfaf79a
ChristianGollmann7 <christian.gollmann7@gmail.com>
2022-05-20 16:08:24 +0000

    added boost options

--- Ex2/src/main.cu
+++ Ex2/src/main.cu
@@ -1,8 +1,36 @@
 #include <iostream>
 
-int main()
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+using OptionsT = boost::program_options::variables_map;
+
+OptionsT parse_options(int ac, char **av)
 {
-    std::cout << "Hello, World" << std::endl;
+    po::options_description desc("Allowed options");
+
+    // clang-format off
+    desc.add_options()
+        ("help,h", "produce help message");
+
+    // clang-format on
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        exit(EXIT_SUCCESS);
+    }
+
+    return vm;
+}
+
+int main(int ac, char **av)
+{
+    OptionsT options = parse_options(ac, av);
     
     return 0;
 }
