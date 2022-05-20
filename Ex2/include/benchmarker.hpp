3e1b74beb8d13f993cb8eb773566cf683262f57a
hitimr <hitimr@gmail.com>
2022-05-20 23:40:49 +0200

    adde sum to benchmarker

--- Ex2/include/benchmarker.hpp
+++ Ex2/include/benchmarker.hpp
@@ -1,9 +1,9 @@
 #include <chrono>
+#include <cmath>
 #include <iostream>
 #include <list>
 #include <map>
 #include <string>
-#include <cmath>
 
 class Timer
 {
@@ -45,29 +45,18 @@ public:
     std::map<std::string, std::list<double>>::iterator itr;
     for (itr = timings.begin(); itr != timings.end(); itr++)
     {
-      double avg = average(itr->first);
-      double std_dev = std_deviation(itr->first, avg);
-      std::cout << itr->first << "\tµ=" << avg << "\tsigma=" << std_dev << std::endl;
+      auto tag = itr->first;
+      std::cout << tag << "s\tµ=" << average(tag) << "s\tsigma=" << std_deviation(tag)
+                << "s\ttotal=" << sum(tag) << std::endl;
     }
   }
 
-  double average(std::string tag)
+  double average(std::string tag) { return sum(tag) / (double)timings[tag].size(); }
+
+  double std_deviation(std::string tag)
   {
     auto data = timings[tag];
-    double average = 0;
-
-    for (double element : data)
-    {
-      average += element;
-    }
-
-    average /= (double)data.size();
-    return average;
-  }
-
-  double std_deviation(std::string tag, double avg)
-  {
-    auto data = timings[tag];
+    double avg = average(tag);
     double sigma2 = 0;
 
     for (double element : data)
@@ -77,5 +66,15 @@ public:
 
     double sigma = sqrt(sigma2) / (double)data.size();
     return sigma;
+  }
+
+  double sum(std::string tag)
+  {
+    double sum = 0;
+    for (double element : timings[tag])
+    {
+      sum += element;
+    }
+    return sum;
   }
 };
