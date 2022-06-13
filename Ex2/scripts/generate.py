fca7f56158824150a2daaa9ae333fe202a8dd896
ChristianGollmann7 <christian.gollmann7@gmail.com>
2022-06-13 09:25:19 +0000

    benchmark data generation is done

--- Ex2/scripts/generate.py
+++ Ex2/scripts/generate.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import igraph as ig
+import time
 
 import common
 
@@ -82,7 +83,32 @@ def calculate_mst(g: ig.Graph):
     """
     return g.spanning_tree(weights=g.get_edge_dataframe()["weight"])
 
+def create_graphs(graph_list, output_dir=common.INPUT_DATA_DIR, create_mst=True):
+    for graph in graph_list:
 
+        base_filename = output_dir / \
+        f"barabasi_{graph['n_nodes']}_{graph['density']}" 
+
+        # generate Connected, weighted graph
+        print("generating graph... n_nodes: {:.1e}, connectivity: {:.3e}".format(graph['n_nodes'], graph['connectivity']))
+        start_time = time.time()
+        g = generate_graph(graph['n_nodes'], graph['connectivity'], w_min=graph['w_min'], w_max=graph['w_max'])
+        print("graph generated in {:.2f} seconds".format(time.time() - start_time))
+
+        # save graph and mst to file
+        print("exporting...")
+        export_graph(g, f"{base_filename}.csv")
+        print("export done after {:.2f} seconds".format(time.time() - start_time))
+
+        # calculate reference solution using igraph
+        if create_mst:
+            start_time = time.time()
+            print("calculating mst...")
+            g_mst_gt = calculate_mst(g)
+            export_graph(g_mst_gt, f"{base_filename}_mst_gt.csv")
+            print("mst done after {:.2f} seconds".format(time.time() - start_time))
+
+'''
 if __name__ == "__main__":
     n_nodes = 500000
     connectivity = 1000
@@ -99,3 +125,29 @@ if __name__ == "__main__":
     # save graph and mst to file
     export_graph(g, f"{base_filename}.csv")
     export_graph(g_mst_gt, f"{base_filename}_mst_gt.csv")
+'''
+
+
+if __name__ == "__main__":
+
+    d_fac = {"10pct": .053, "50pct": .293, "90pct": .684} # these factors will create the desired densities
+
+    ranges = {"10pct": [100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600],
+              "50pct": [50, 100, 200, 400, 800, 1600, 3200, 6400, 12800],
+              "90pct": [25, 50, 100, 200, 400, 800, 1600, 3200, 6400]    
+             }
+
+    #for key in d_fac:
+    for key in ['90pct']:
+        print("density: ", key)
+        graph_list = []
+        for r in ranges[key]:
+            graph_list.append({"n_nodes": r, "connectivity": int(d_fac[key]*r), "w_min": 1, "w_max": 1000, 'density': key})
+        print('list of graphs to be created:')
+        for graph in graph_list:
+            print(graph)
+        print('')
+
+        out_dir = common.WORKSPACE_DIR / "benchmark_data"
+        print('creating graphs: ')
+        create_graphs(graph_list, output_dir=out_dir, create_mst=True)
